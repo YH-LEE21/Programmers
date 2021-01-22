e927c5b78863130800de14c19462443e2d3e621a
programmers <73163074+YH-LEE21@users.noreply.github.com>
2021-01-22 13:16:10 +0900

    Programmers 문자열 다루기 기본

---
+++ src/main/java/level1/문자열 다루기 기본/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        if(4==s.length()||s.length()==6){
+           for(int i=0;i<s.length();i++){
+               if(48>s.charAt(i)||s.charAt(i)>57){
+                   answer=false;
+               }
+           }
+        }
+        else{
+            answer=false;
+        }
+        return answer;
+    }
+}
