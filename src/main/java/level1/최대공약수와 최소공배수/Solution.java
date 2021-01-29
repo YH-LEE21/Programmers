e1081aaa1f737488ac55354ad66d7972c0c74eeb
YOHAN <73163074+YH-LEE21@users.noreply.github.com>
2021-01-29 23:56:26 +0900

    programmers 최대공약수와 최소공배수

--- src/main/java/level1/최대공약수와 최소공배수/Solution.java
+++ src/main/java/level1/최대공약수와 최소공배수/Solution.java
@@ -1,7 +1,8 @@
 class Solution {
     public int[] solution(int n, int m) {
         int[] answer = new int[2];
-        int max=Math.max(n,m);
+        int max = Math.max(n,m);
+            // int max= n>m ? n: m
         for(int i=1;i<=max;i++){
             if(n%i==0&&m%i==0){
                 answer[0]=i;
