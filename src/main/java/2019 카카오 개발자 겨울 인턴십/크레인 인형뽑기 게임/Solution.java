91c307f826f4ae0477fffe2e8440b967ff746eb4
YOHAN <73163074+YH-LEE21@users.noreply.github.com>
2021-02-08 15:41:44 +0900

    Create Solution.java

---
+++ src/main/java/2019 카카오 개발자 겨울 인턴십/크레인 인형뽑기 게임/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+class Soultion{
+    public int solution(int[] board,int[] moves){
+          int answer = 0;
+          Stack<Integer> stack = new Stack<Integer>();
+          for(int move : moves){
+              int m = move - 1;
+              for(int i=0;i<board.length;i++){
+                  int x = board[i][m];
+                  if(x == 0)continue;
+                  if(!stack.isEmpty&&stack.peek()==x){
+                      stack.pop();
+                      answer++;//터진 횟수
+                  }
+                  else{
+                      stack.push();
+                  }
+                  board[i][m] = 0;
+                  break;
+              }
+          }
+          return answer*2;//터진횟수에 x2 를 해주어 몇개가  확인한다.
+    }
+}
